313ce72b924268215d736211117b55acba47300e
Taburetk <Taburetk@users.noreply.github.com>
2018-05-29 21:39:00 +0300

    ShopMenuUpdate
    Add prototype of shop menu

---
+++ Assets/Scripts/ShopLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Animations;
+
+public class ShopLogic : MonoBehaviour, IPointerDownHandler, IPointerClickHandler, IPointerUpHandler{
+	Animator a;
+	[SerializeField] Text text;
+	
+	void Start() {
+		a = gameObject.GetComponent<Animator>();
+	}
+
+	public void OnPointerDown (PointerEventData ped) {
+		a.SetBool("isPressed", true);
+	}
+	public void OnPointerClick(PointerEventData ped) {
+		Debug.Log("You have pressed on this button");
+	}
+
+	public void OnPointerUp(PointerEventData ped) {
+		a.SetBool("isPressed", false);
+	}
+}
